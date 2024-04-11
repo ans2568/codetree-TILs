91ce471384c8cb7071efa009f3f9503c5562c439
Sung won An <80823431+ans2568@users.noreply.github.com>
2024-04-11 16:57:05 +0900

    Title: 왕실의 기사 대결, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240411/왕실의 기사 대결/royal-knight-duel.cpp
+++ 240411/왕실의 기사 대결/royal-knight-duel.cpp
@@ -11,77 +11,19 @@ struct Knight {
     int k; // 체력
     int damaged; // 받은 데미지
 };
+
 int L, N, Q;
-int game[50][50];
+int game[41][41];
 int dy[4] = {-1, 0, 1, 0};
 int dx[4] = {0, 1, 0, -1};
-bool killed[50];
-bool pulled[50];
-map<int, Knight> knights;
+bool killed[31];
+bool pulled[31];
 bool result;
+map<int, Knight> knights;
+
 void input();
-
-bool move(int idx, int direction, vector<pair<int, pair<int, int>>>& vec) {
-    // cout << idx << "번째 기사 이동 시작" << endl;
-    int y = knights[idx].y;
-    int x = knights[idx].x;
-    int w = knights[idx].w;
-    int h = knights[idx].h;
-    int nx = x + dx[direction];
-    int ny = y + dy[direction];
-    // 기사의 좌표를 포함한 모든 방패에서
-    for (int r=ny; r<=ny+h-1; ++r) {
-        for (int c=nx; c<=nx+w-1; ++c) {
-            // cout << idx << "번째 기사 " << r << ", " << c << " 좌표 확인" << endl;
-            //벽이거나 맵 밖이라면
-            if (game[r][c] == 2 || r <= 0 || r > L || c <= 0 || c > L) {
-                return false;
-            }
-            for (int i=1; i<=N; ++i) {
-                if (i == idx) continue;
-                if (killed[i]) continue;
-                // 이미 존재하는 기사 있다면
-                if ((r >= knights[i].y && r <= (knights[i].y + knights[i].h - 1)) && 
-                    (c >= knights[i].x && c <= (knights[i].x + knights[i].w - 1))) {
-                    // 밀쳤는데 이미 있던 기사가 움직이지 않았다면
-                    if (!move(i, direction, vec) || !result) {
-                        result = false;
-                        return false;
-                    }
-                    // cout << i << "번째 기사 밀쳐짐" << endl;
-                    pulled[i] = true;
-                }
-            }
-        }
-    }
-    vec.push_back(make_pair(idx, make_pair(ny, nx)));
-    // knights[idx].y = ny;
-    // knights[idx].x = nx;
-    return true;
-}
-
-void damage(int idx) {
-    for (int i=1; i<=N; ++i) {
-        if (i == idx) continue;
-        if (killed[i]) continue;
-        if (!pulled[i]) continue;
-        auto k = knights[i];
-        for (int y=k.y; y<=k.y+k.h-1; ++y) {
-            for (int x=k.x; x<=k.x+k.w-1; ++x) {
-                if (killed[i]) continue;
-                if (game[y][x] == 1) {
-                    knights[i].k -= 1;
-                    if (knights[i].k <= 0) {
-                        killed[i] = true;
-                        continue;
-                    }
-                    knights[i].damaged += 1;
-                    // cout << i << "번째 기사 받은 데미지 + 1 = " << knights[i].damaged << endl;
-                }
-            }
-        }
-    }
-}
+bool move(int idx, int direction, vector<pair<int, pair<int, int>>>& vec);
+void damage(int idx);
 
 int main() {
     input();
@@ -122,21 +64,74 @@ void input() {
         int idx, d;
         cin >> idx >> d;
         if (killed[idx]) {
-            // cout << idx << "번째 기사 죽음" << endl;
             continue;
         } 
             
         // 기사 이동
         vector<pair<int, pair<int, int>>> vec;
         if (!move(idx, d, vec)) {
-            // cout << idx << "번째 기사가 밀쳤으나 벽이나 밖이라 움직이지 않음" << endl;
             continue;
         }
         for (int j=0; j<vec.size(); ++j) {
-            // cout << vec[j].first << "번째 기사 좌표 변환" << endl;
             knights[vec[j].first].y = vec[j].second.first;
             knights[vec[j].first].x = vec[j].second.second;
         }
         damage(idx);
+    }
+}
+
+bool move(int idx, int direction, vector<pair<int, pair<int, int>>>& vec) {
+    int y = knights[idx].y;
+    int x = knights[idx].x;
+    int w = knights[idx].w;
+    int h = knights[idx].h;
+    int nx = x + dx[direction];
+    int ny = y + dy[direction];
+    // 기사의 좌표를 포함한 모든 방패에서
+    for (int r=ny; r<=ny+h-1; ++r) {
+        for (int c=nx; c<=nx+w-1; ++c) {
+            //벽이거나 맵 밖이라면
+            if (game[r][c] == 2 || r <= 0 || r > L || c <= 0 || c > L) {
+                return false;
+            }
+            for (int i=1; i<=N; ++i) {
+                if (i == idx) continue;
+                if (killed[i]) continue;
+                // 이미 존재하는 기사 있다면
+                if ((r >= knights[i].y && r <= (knights[i].y + knights[i].h - 1)) && 
+                    (c >= knights[i].x && c <= (knights[i].x + knights[i].w - 1))) {
+                    // 밀쳤는데 이미 있던 기사가 움직이지 않았다면
+                    if (!move(i, direction, vec) || !result) {
+                        result = false;
+                        return false;
+                    }
+                    pulled[i] = true;
+                }
+            }
+        }
+    }
+    vec.push_back(make_pair(idx, make_pair(ny, nx)));
+    return true;
+}
+
+void damage(int idx) {
+    for (int i=1; i<=N; ++i) {
+        if (i == idx) continue;
+        if (killed[i]) continue;
+        if (!pulled[i]) continue;
+        auto k = knights[i];
+        for (int y=k.y; y<=k.y+k.h-1; ++y) {
+            for (int x=k.x; x<=k.x+k.w-1; ++x) {
+                if (killed[i]) continue;
+                if (game[y][x] == 1) {
+                    knights[i].k -= 1;
+                    if (knights[i].k <= 0) {
+                        killed[i] = true;
+                        continue;
+                    }
+                    knights[i].damaged += 1;
+                }
+            }
+        }
     }
 }
